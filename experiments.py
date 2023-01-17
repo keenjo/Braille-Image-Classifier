db10965a32ad55ab9b4979406fff51ccd6144861
Karolin <boczon1u@etu.univ-lorraine.fr>
2023-01-17 17:03:46 +0100

    refine experiments.py

--- experiments.py
+++ experiments.py
@@ -8,44 +8,24 @@ import copy
 from model import CNNClassif, init_weights
 from data_prep import ImageDataset
 from sacred import Experiment
+from sacred.observers import FileStorageObserver
 
 ex = Experiment('braille_cnn')
+ex.observers.append(FileStorageObserver('runs'))
+
 
 @ex.config
 def config():
     """Configuration of the Braille Image Classifier experiment."""
     seed = 54
     batch_size = 8
-    num_epochs = 1
-    loss_fn = nn.CrossEntropyLoss()
+    num_epochs = 30
+    loss_fn=nn.CrossEntropyLoss()
     learning_rate = 0.01
 
 
 @ex.capture
-def check_batch_shape(dataloader):
-    """Checking that the data has the correct shape"""
-    batch_data, batch_name = next(iter(dataloader))
-    print(f'Batch shape [batch_size, image_shape]: {batch_data.shape}')
-    print('Number of batches:', len(dataloader))
-
-@ex.capture
-def number_of_labels(train_data):
-    labels = []
-    for x in train_data:
-        labels.append(x[1])
-    
-    num_classes = len(list(set(labels)))
-    return num_classes
-
-@ex.capture
-def number_of_paramenters(model):
-    print('Total number of parameters: ', sum(p.numel() for p in model.parameters()))
-
-
-# Training function
 def training_cnn_classifier(model, train_dataloader, num_epochs, loss_fn, learning_rate, verbose=True):
-
-    # Making a copy of the model
     model_tr = copy.deepcopy(model)
     model_tr.train()
     
@@ -70,11 +50,11 @@ def training_cnn_classifier(model, train_dataloader, num_epochs, loss_fn, learni
         loss_all_epochs.append(loss_current_epoch / (batch_index + 1))
         if verbose:
             print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss_current_epoch/(batch_index + 1):.4f}')
+            ex.log_scalar('loss', loss_current_epoch, step=epoch+1)
         
     return model_tr, loss_all_epochs
 
 
-# Evaluation function
 def eval_cnn_classifier(model, eval_dataloader):
 
     model.eval() 
@@ -101,21 +81,33 @@ def run(seed, batch_size, num_epochs, loss_fn, learning_rate):
     split_data = random_split(dataset, [1248, 156, 156], generator=torch.Generator().manual_seed(seed))
     train_data, val_data, test_data = split_data
 
-    # Creating the dataloaders
     train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
-    num_classes = number_of_labels(train_dataloader)
 
+    num_classes = len(list(set([x[1] for x in train_dataloader]))) 
+    print("Number of classes: ", num_classes)
+    batch_data, batch_name =  next(iter(train_dataloader))
+    print(f'Batch shape [batch_size, image_shape]: {batch_data.shape}')
+    print('Number of batches:', len(train_dataloader))
+
+    print("== Initializing model...")
     model = CNNClassif(16, 32, 64, num_classes)
-    number_of_paramenters(model)
-    torch.manual_seed(0)
-    model.apply(init_weights)
+    num_params = sum(p.numel() for p in model.parameters())
+    ex.log_scalar('number_of_params', num_params)
+    print(model)
 
-    model, loss_total = training_cnn_classifier(model, train_dataloader, num_epochs, loss_fn, learning_rate, verbose=True)
+    print("== Training...")
+    model, loss_total = training_cnn_classifier(model, train_dataloader)
     torch.save(model.state_dict(), 'test_model.pt')
-    #plt.plot(loss_total)
-    #plt.show()
+    ex.add_artifact('test_model.pt')
 
+    # TO DO: make it prettier
+    plt.plot(list(range(num_epochs)), loss_total)
+    plt.savefig('loss.png')
+    ex.add_artifact('loss.png')
+
+    print("== Evaluating...")
     accuracy = eval_cnn_classifier(model, test_dataloader)
-    return accuracy
+    ex.log_scalar('accuracy', accuracy)
+    return round(accuracy, 2)
